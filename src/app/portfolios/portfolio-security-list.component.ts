fdf98e658669dc88809be84f2ca154938573c0e5
Onkar Tendulkar <OTendulkar@dstworldwideservices.com>
2019-09-24 13:25:13 +0530

    Filtering for securities

--- src/app/portfolios/portfolio-security-list.component.ts
+++ src/app/portfolios/portfolio-security-list.component.ts
@@ -7,5 +7,36 @@ import { IPortfolioSecurity } from './shared/portfolio-security.model';
 })
 export class PortfolioSecuritiesListComponent
 {
-    @Input() securities:IPortfolioSecurity;
+    @Input() securities:IPortfolioSecurity[];
+    
+    visibleSecurities:IPortfolioSecurity[];
+
+    sectors : string[] = ["All"];
+
+    ngOnInit()
+    {
+        if(this.securities)
+        {
+            this.sectors = ["All"];
+            this.sectors = this.sectors.concat(this.securities.map(s => s.sector));
+        }
+        this.visibleSecurities = this.securities.slice(0);
+        console.log(this.sectors);
+    }
+
+    onFilterChange(filter)
+    {
+        console.log(filter);
+        if(filter=="All")
+        {
+            this.visibleSecurities = this.securities.slice(0);
+        }
+        else
+        {
+            this.visibleSecurities = this.securities.filter(s => 
+                {
+                    return s.sector.toUpperCase() === filter.toUpperCase();
+                });
+        }
+    }
 }
